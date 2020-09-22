aca8a06537979262858371a77d803b38dd38df9a
RyanShim2000 <49270895+RyanShim2000@users.noreply.github.com>
2020-09-21 23:13:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/form.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "2713fd06941d8e3fccbddc5cc138f2bf";
+
+class App extends React.Component {
+  state = {
+    city: [],
+    temp: [],
+    humidity: [],
+    wind: [],
+    description: []
+  };
+
+  weather = async event => {
+    event.preventDefault();
+    const place = event.target.city.value;
+    const link =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      place +
+      "&appid=" +
+      API +
+      "&units=metric";
+
+    const apiLink = await fetch(link);
+
+    const data = await apiLink.json();
+    if (place) {
+      this.setState({
+        city: "City: " + data.name,
+        temp: "Temperature: " + data.main.temp + " degrees Celsius",
+        humidity: "Humidity: " + data.main.humidity + " %",
+        wind: "Windspeed: " + data.wind.speed + " m/s",
+        description: "Description: " + data.weather[0].main
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <h1>WEATHER APP</h1>
+        </div>
+
+        <div className="row justify-content-center">
+          <Form weather={this.weather} />
+        </div>
+        <div className="el">{this.state.city}</div>
+        <div className="el">{this.state.temp}</div>
+        <div className="el">{this.state.humidity}</div>
+        <div className="el">{this.state.wind}</div>
+        <div className="el">{this.state.description}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
