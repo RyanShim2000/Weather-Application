0df0e026fb16fca0d2b3fdb6c15679d3f6c2806f
RyanShim2000 <49270895+RyanShim2000@users.noreply.github.com>
2020-09-21 23:22:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Form from "./components/form.js";
+import Form from "./form.js";
 
 const API = "2713fd06941d8e3fccbddc5cc138f2bf";
 
