9323378d3d73b05b6494ee1f251d12c07b4769ef
RyanShim2000 <49270895+RyanShim2000@users.noreply.github.com>
2020-09-21 23:20:25 -0400

    Update form.js

--- src/form.js
+++ src/form.js
@@ -1,1 +1,24 @@
+import React from "react";
+import "./form.css";
+
+class Form extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.weather} className=" col-12 d-flex ">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="city"
+            name="city"
+          />
+          <button className="btn ">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
 
